f78800a431590992f693bb15cd5d54f85291546f
peaceandiago <paola.santiago@mail.utoronto.ca>
2016-06-29 15:58:56 -0400

    """ This program is to clean the data for dataset """
    #finished 3 functions - currently trying to find a way to create percentages

--- PycharmProjects/TRIEC_data_analysis/clean_data.py
+++ PycharmProjects/TRIEC_data_analysis/clean_data.py
@@ -4,39 +4,41 @@ This program is to clean the data for dataset
 import csv
 
 
+""" These three functions help solve: Occupation of mentees/mentors registered per fiscal year"""
+
 data_out = open("mentees_all_attributes.csv", "rU")
 reader = csv.reader(data_out)
 next(reader,None)
 
-counters = {}
 
-""" These three functions help solve: Occupation of mentees/mentors registered per fiscal year"""
 def participated():
-    for line in reader: 
+    counters = {}
+    for line in reader:
         if int(line[6]) > 0:
             primary = line[7].strip('[]')
             counters.setdefault(primary, 0)
             counters[primary] += 1
 
-    for k, v in counters.items():
-        print(k, v)
+    # for k, v in counters.items():
+    #     print k,v
+    return counters
 
-participated()
 
 data_out.seek(0)
 reader = csv.reader(data_out)
 next(reader,None)
 
 def notParticipated():
+    counters = {}
     for line in reader:
         if int(line[6]) == 0:
             primary = line[7].strip('[]')
             counters.setdefault(primary, 0)
             counters[primary] += 1
 
-    for k, v in counters.items():
-        print(k, v)
-notParticipated()
+    # for k, v in counters.items():
+    #     print k,v
+    return counters
 
 data_out.seek(0)
 reader = csv.reader(data_out)
@@ -44,15 +46,23 @@ next(reader,None)
 
 
 def generalNumber():
+    counters = {}
     for line in reader:
-        primary = line[7].strip('[]')
-        counters.setdefault(primary, 0)
-        counters[primary] += 1
+        if int(line[6]) >= 0:
+            primary = line[7].strip('[]')
+            counters.setdefault(primary, 0)
+            counters[primary] += 1
 
-    for k, v in counters.items():
-        print(k, v)
+    # for k, v in counters.items():
+    #     print k, v
+    return counters
 
-notParticipated()
+counter_p = participated()
+#print counter_p
+counter_g = generalNumber()
+print counter_g
+#success_percentage = [counter_p.get(primary)/counter_g.get(primary, 1) for primary in counter_p]
+#print success_percentage
 
 
 data_out.close()
