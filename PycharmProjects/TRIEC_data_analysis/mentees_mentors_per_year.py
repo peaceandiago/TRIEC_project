ca89b387b5212e27eda14bb3a20361c11086b1e5
peaceandiago <paola.santiago@mail.utoronto.ca>
2016-06-30 16:31:44 -0400

    """ This program aims to clean the dataset to answer the question "Occupations of mentees/mentors registered per fiscal year" """
    1.) remove time from date list
    
    THINGS TO DO:
    
    1.) Get variable for range for dates to get fiscal years
    2.) IF-Conditionals
    3.) list all the jobs/industries within each fiscal year
    4.) create dictionary to show for each fiscal year and the job/industries for each

--- PycharmProjects/TRIEC_data_analysis/mentees_mentors_per_year.py
+++ PycharmProjects/TRIEC_data_analysis/mentees_mentors_per_year.py
@@ -2,12 +2,9 @@
 This program aims to clean the dataset to answer the question "Occupations of mentees/mentors registered per fiscal year"
 """
 import csv
+from datetime import timedelta, date
 
-# file_in = open("mentees_all_attributes.csv", "rU")
-# reader = csv.reader(file_in)
-# next(file_in,None)
-#
-def isolateDate(csv_file): #remove time element from data
+def isolateDate(csv_file): #remove time element from dataset
     file_in = open(csv_file, "rU")
     reader = csv.reader(file_in)
     next(file_in,None)
@@ -18,6 +15,27 @@ def isolateDate(csv_file): #remove time element from data
         new_date.append(date[0])
     return new_date
 
-mentee_date = isolateDate("mentees_all_attributes.csv")
-print mentee_date
+mentee_date = isolateDate("mentees_all_attributes.csv") #all dates in the mentee csv
+
+#print mentee_date
+
+def daterange(start_date, end_date):  #create range of dates
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+start_date = date(2005, 4, 1)
+end_date = date(2006, 4, 1)
+for single_date in daterange(start_date, end_date):
+    first_year = single_date
+
+    print first_year
+
+def compare(list1, list2):
+    for value in list1:
+        if value in list2:
+            return value
+
+first_fiscal_year = compare(mentee_date,first_year)
+print first_fiscal_year
+
 
