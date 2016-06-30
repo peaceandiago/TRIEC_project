69fd908b52b08d6dc6407ab0bf74aeaec8919178
peaceandiago <paola.santiago@mail.utoronto.ca>
2016-06-30 13:48:14 -0400

    """ This program is to clean the data for dataset """
    #finished 3 functions to answer question "Who gets matched and who doesn't, specifically in the mentee pool"

--- PycharmProjects/TRIEC_data_analysis/clean_data.py
+++ PycharmProjects/TRIEC_data_analysis/clean_data.py
@@ -4,7 +4,7 @@ This program is to clean the data for dataset - mentees
 import csv
 
 
-""" These three functions help solve: Occupation of mentees/mentors registered per fiscal year"""
+""" These three functions help solve: Who gets matched and who doesn't"""
 
 data_out = open("mentees_all_attributes.csv", "rU")
 reader = csv.reader(data_out)
@@ -17,70 +17,39 @@ def participated(): #Participated in Mentorship
             primary = line[7].strip()
             part_counters.setdefault(primary, 0)
             part_counters[primary] += 1
-
-    # for k, v in part_counters.items():
-    #     print k,v
     return part_counters
 
 
-# data_out.seek(0)
-# reader = csv.reader(data_out)
-# next(reader,None)
+def notParticipated():
+    with open("mentees_all_attributes.csv", "rU") as data_out:
+        reader = csv.reader(data_out)
+        next(reader,None)
+        notpart_counters = {}
+        for line in reader:
+            if int(line[6]) == 0:
+                primary = line[7].strip()
+                notpart_counters.setdefault(primary, 0)
+                notpart_counters[primary] += 1
+    return notpart_counters
 
-# def notParticipated():
-#     notpart_counters = {}
-#     for line in reader:
-#         if int(line[6]) == 0:
-#             primary = line[7].strip()
-#             notpart_counters.setdefault(primary, 0)
-#             notpart_counters[primary] += 1
-#
-#     for k, v in notpart_counters.items():
-#         print k,v
-#     return notpart_counters
-#
-# def generalNumber(): #General number of mentees with their primary occupation
-#     with open("mentees_all_attributes.csv", "rU") as data_out:
-#         reader = csv.reader(data_out)
-#         next(reader,None)
-#         gen_counters = {}
-#         for line in reader:
-#             if int(line[6]) >= 0:
-#                 primary = line[7].strip()
-#                 gen_counters.setdefault(primary, 0)
-#                 gen_counters[primary] += 1
-#
-#         for k, v in gen_counters.items():
-#             print k, v
-    #return gen_counters
-#
-# counter_p = participated()
-# print counter_p #dictionary
-# counter_g = generalNumber()
-# print counter_g #dictionary
-#
-# for key in counter_p.iteritems():
-#     print key
-#
-# def divide(divedends, divisors):
-#     new_dict = ()
-#     for key, counter_g in counter_p.iteritems():
-#         print key
-#         if key in counter_g:
-#             new_dict[key] = counter_p/counter_g
-#         else:
-#             new_dict[key] = counter_p
-#     print new_dict
+def generalNumber(): #General number of mentees with their primary occupation
+    with open("mentees_all_attributes.csv", "rU") as data_out:
+        reader = csv.reader(data_out)
+        next(reader,None)
+        gen_counters = {}
+        for line in reader:
+            if int(line[6]) >= 0:
+                primary = line[7].strip()
+                gen_counters.setdefault(primary, 0)
+                gen_counters[primary] += 1
+    return gen_counters
 
-
-
-# # dict([(k, d2[k] / float(d1[k])) for k in d1 if k in d2])
-# success_percentage = dict([k, counter_p[k] / float(counter_g[k] for k in counter_g if k in counter_p)])
-
-
-
-    #     success_percentage = counter_p[values]/counter_g[values]
-    #     print success_percentage
+counter_p = participated() #creates a mentee who participated/primary occupation dictionary
+counter_np = notParticipated() #creates mentee who did not participate/primary occupation dictionary
+counter_g = generalNumber() #creates general number of mentees/primary occupation dictionary
+success_percentage = dict((k, counter_p[k] / float(counter_g[k])) for k in counter_p if k in counter_g) #calculates participants/general
+for key,value in success_percentage.items():  #converts into percentage
+    success_percentage[key] = value * 100
 
 
 data_out.close()
