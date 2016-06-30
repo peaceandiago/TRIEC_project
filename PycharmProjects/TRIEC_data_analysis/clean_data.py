9aa6159e648900204775931a3d41f6caa094a253
peaceandiago <paola.santiago@mail.utoronto.ca>
2016-06-30 11:01:17 -0400

    """ This program is to clean the data for dataset """
    #finished 3 functions - currently trying to find a way to create percentages

--- PycharmProjects/TRIEC_data_analysis/clean_data.py
+++ PycharmProjects/TRIEC_data_analysis/clean_data.py
@@ -1,5 +1,5 @@
 """
-This program is to clean the data for dataset
+This program is to clean the data for dataset - mentees
 """
 import csv
 
@@ -10,8 +10,7 @@ data_out = open("mentees_all_attributes.csv", "rU")
 reader = csv.reader(data_out)
 next(reader,None)
 
-
-def participated():
+def participated(): #Participated in Mentorship
     part_counters = {}
     for line in reader:
         if int(line[6]) > 0:
@@ -19,51 +18,69 @@ def participated():
             part_counters.setdefault(primary, 0)
             part_counters[primary] += 1
 
-    # for k, v in counters.items():
+    # for k, v in part_counters.items():
     #     print k,v
     return part_counters
 
 
-data_out.seek(0)
-reader = csv.reader(data_out)
-next(reader,None)
+# data_out.seek(0)
+# reader = csv.reader(data_out)
+# next(reader,None)
 
-def notParticipated():
-    notpart_counters = {}
-    for line in reader:
-        if int(line[6]) == 0:
-            primary = line[7].strip()
-            notpart_counters.setdefault(primary, 0)
-            notpart_counters[primary] += 1
+# def notParticipated():
+#     notpart_counters = {}
+#     for line in reader:
+#         if int(line[6]) == 0:
+#             primary = line[7].strip()
+#             notpart_counters.setdefault(primary, 0)
+#             notpart_counters[primary] += 1
+#
+#     for k, v in notpart_counters.items():
+#         print k,v
+#     return notpart_counters
+#
+# def generalNumber(): #General number of mentees with their primary occupation
+#     with open("mentees_all_attributes.csv", "rU") as data_out:
+#         reader = csv.reader(data_out)
+#         next(reader,None)
+#         gen_counters = {}
+#         for line in reader:
+#             if int(line[6]) >= 0:
+#                 primary = line[7].strip()
+#                 gen_counters.setdefault(primary, 0)
+#                 gen_counters[primary] += 1
+#
+#         for k, v in gen_counters.items():
+#             print k, v
+    #return gen_counters
+#
+# counter_p = participated()
+# print counter_p #dictionary
+# counter_g = generalNumber()
+# print counter_g #dictionary
+#
+# for key in counter_p.iteritems():
+#     print key
+#
+# def divide(divedends, divisors):
+#     new_dict = ()
+#     for key, counter_g in counter_p.iteritems():
+#         print key
+#         if key in counter_g:
+#             new_dict[key] = counter_p/counter_g
+#         else:
+#             new_dict[key] = counter_p
+#     print new_dict
 
-    # for k, v in counters.items():
-    #     print k,v
-    return notpart_counters
-
-data_out.seek(1)
-reader = csv.reader(data_out)
-next(reader,None)
 
 
-def generalNumber():
-    gen_counters = {}
-    for line in reader:
-        if int(line[6]) >= 0:
-            primary = line[7].strip()
-            gen_counters.setdefault(primary, 0)
-            gen_counters[primary] += 1
+# # dict([(k, d2[k] / float(d1[k])) for k in d1 if k in d2])
+# success_percentage = dict([k, counter_p[k] / float(counter_g[k] for k in counter_g if k in counter_p)])
 
-    for k, v in gen_counters.items():
-        print k, v
-    return gen_counters
 
-counter_p = participated()
-print counter_p
-counter_g = generalNumber()
-print counter_g
-# print counter_g
-#success_percentage = [counter_p.get(primary)/counter_g.get(primary, 1) for primary in counter_p]
-#print success_percentage
+
+    #     success_percentage = counter_p[values]/counter_g[values]
+    #     print success_percentage
 
 
 data_out.close()
