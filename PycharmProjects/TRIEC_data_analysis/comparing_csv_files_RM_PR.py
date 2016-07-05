1421f9842b37053f9361ed6a00e95c1df39f17d3
peaceandiago <paola.santiago@mail.utoronto.ca>
2016-07-05 16:44:41 -0400

    Compares 2 CSV files together

--- PycharmProjects/TRIEC_data_analysis/comparing_csv_files_RM_PR.py
+++ PycharmProjects/TRIEC_data_analysis/comparing_csv_files_RM_PR.py
@@ -1,31 +1,42 @@
 import csv
 
+def findAcceptance():
+    """
+    This function identifies the mentors, mentees, and RMdates under the condition 'Acceptance'
+    :return: an array for mentors, mentees, RMdate
+    """
+    with open ("pastRMs.csv", "r") as RM_file:
+        RM_reader = csv.reader(RM_file)
+        next(RM_reader, None)
+        RM = []
+        for rows in RM_reader:
+            if rows[16] == "Accepted":
+                reports_mentors = rows[0].title()
+                reports_mentees = rows[7].title()
+                RM_date = rows[15]
+                Accepted_Cases = reports_mentors, reports_mentees, RM_date         #Put this all in one list
+                RM_list = list(Accepted_Cases)
+                RM.append(RM_list)
+        return RM
 
-with open ("pastRMs.csv", "r") as RM_file:
-    RM_reader = csv.reader(RM_file)
-    next(RM_reader, None)
-    RM = []
-    for rows in RM_reader:
-        if rows[16] == "Accepted":
-            reports_mentors = rows[0].title()
-            reports_mentees = rows[7].title()
-            RM_date = rows[15]
-            Accepted_Cases = reports_mentors, reports_mentees, RM_date         #Put this all in one list
-            RM_list = list(Accepted_Cases)
-            RM.append(RM_list)
+RM_lists = findAcceptance()
 
-RM_lists = RM
+def neededData():
+    """
+    This function takes the identified list from the findAcceptance() and compare it with another csv to get the necessary data
+    :return: Start_date, partnership_mentors, partnership_mentees, rmrows[2]
+    """
 
-with open("partnership_report.csv", "r") as PR_file:
-    PR_reader = csv.reader(PR_file)
-    next(PR_file, None)
-    for rows in PR_reader:
-        partnership_mentors = rows[2].title()
-        partnership_mentees = rows[5].title()
-        for rmrows in RM_lists:
-            if partnership_mentees in rmrows[1] and partnership_mentors in rmrows[0]:
-                Start_date = rows[0]
-                print Start_date, partnership_mentors, partnership_mentees, rmrows[2]
+    with open("partnership_report.csv", "r") as PR_file:
+        PR_reader = csv.reader(PR_file)
+        next(PR_file, None)
+        for rows in PR_reader:
+            partnership_mentors = rows[2].title()
+            partnership_mentees = rows[5].title()
+            for rmrows in RM_lists:
+                if partnership_mentees in rmrows[1] and partnership_mentors in rmrows[0]:
+                    Start_date = rows[0]
+                return Start_date, partnership_mentors, partnership_mentees, rmrows[2]
 
 
 
