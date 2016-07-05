e324234a2ff2bbc532ced435784d5e37eda007fe
peaceandiago <paola.santiago@mail.utoronto.ca>
2016-07-05 09:13:25 -0400

    """ This program aims to summarize the RM dataset to gather insights: summary of the reject reasons for the last 3 years # of days between RM date and the date when RM accept/reject * % of expired RMs compare to the total number of RM generated Average length of time for matches in NOCS that TMP matches * The # of days between RM accept and partnership start date * """

--- PycharmProjects/TRIEC_data_analysis/RM_summary.py
+++ PycharmProjects/TRIEC_data_analysis/RM_summary.py
@@ -12,32 +12,26 @@ import datetime
 from datetime import timedelta, date
 
 
-def rejectionSummary(line_status):
-    """Counts the number of occurence for each rejected reason - returns count of rejected reasons"""
-    file = open("pastRMs.csv", "r")
-    reader = csv.reader(file)
-    next(reader,None)
+def rejectionSummary():
+    """Counts the number of occurence for each rejected reason - returns count of rejected reasons
+    compares the date ranges that are inputted with the date in the csv file - returns status"""
     rej_summary = {}
-    for line in reader:
-        if line_status == 'Rejected':
-            rejection = line[18]
-            rej_summary.setdefault(rejection, 0)
-            rej_summary[rejection] += 1
-
-    return rej_summary
-
-
-def dateRanges(YYYY, MM, D):
-    """compares the date ranges that are inputted with the date in the csv file - returns status"""
-    start_date = datetime.date(YYYY, MM, D)
-    end_date = datetime.date(YYYY, MM, D)
+    start_date = datetime.date(2014, 04, 1)
+    end_date = datetime.date(2015, 03, 31)
     file = open("pastRMs.csv", "r")
     reader = csv.reader(file)
     next(reader, None)
     for line in reader:
         date = datetime.datetime.strptime(line[15], '%d/%m/%Y').date()
         if start_date <= date <= end_date:
-            return line[18]
+            if line[16] == 'Rejected':
+                rejection = line[18]
+                rej_summary.setdefault(rejection, 0)
+                rej_summary[rejection] += 1
+    return rej_summary
+
+print rejectionSummary()
+
 
 
 
