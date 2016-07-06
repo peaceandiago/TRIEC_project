d8b972f5eff7234c1e130f2adc433040b5171525
peaceandiago <paola.santiago@mail.utoronto.ca>
2016-07-06 10:51:22 -0400

    Compares 2 CSV files together
    1.) Isolated data under condition "Acceptance"
    2.) Matched RM = PR
    3.) Isolated, converted, transformed dates
    4.) Subtracted Dates and counted occurence

--- PycharmProjects/TRIEC_data_analysis/comparing_csv_files_RM_PR.py
+++ PycharmProjects/TRIEC_data_analysis/comparing_csv_files_RM_PR.py
@@ -51,74 +51,50 @@ data = neededData()
 def isolateDate():
     """
     This function isolates the two dates that are needed and puts them into a consistent format and converts them into objects instead of string
-    :return: [start_date], [rm_date]
+    then subtract the start date from the RM date and convert it back to the string to make it a list of strings
+    :return: days between RM and Start_dates
     """
+    DAYS = []
     for rows in data:
-        if rows[0] and rows[1] and rows[2] and rows[3] is not None: #remove the rows with missing data
+        if rows[0] and rows[1] and rows[2] and rows[3] is not None:  # remove the rows with missing data
             format_2 = '%d %m %Y'
-            start_date = datetime.datetime.strptime(rows[0], '%d-%b-%y').strftime(format_2)
-            rm_date = datetime.datetime.strptime(rows[3], '%d/%m/%Y').date().strftime(format_2)
+            start_date = datetime.datetime.strptime(rows[0], '%d-%b-%y')
+            rm_date = datetime.datetime.strptime(rows[3], '%d/%m/%Y')
+            delta = start_date - rm_date
+            number_of_days_rm_pr = delta.days
+            numbers = str(number_of_days_rm_pr)
+            DAYS.append(numbers)
+    return DAYS
+
+DAYS = isolateDate()
+
+def countDays():
+    days_summary = {}
+    for rows in DAYS:
+        days_summary.setdefault(rows, 0)
+        days_summary[rows] += 1
+    return days_summary
+
+print countDays()
 
 
-
-# with open ("pastRMs.csv", "r") as RM_file:
-#     RM_reader = csv.reader(RM_file)
-#     next(RM_reader, None)
-#     RM_Mentor_List = []
-#     RM_Mentee_List = []
-#     for rows in RM_reader:
-#         if rows[16] == "Accepted":
-#             reports_mentors = rows[0].title()
-#             reports_mentees = rows[7].title()
-#             RM_date = rows[15]
-#             print reports_mentees, reports_mentors, RM_date
-#
-#
-
-
-#             RM_Mentor_List.append(reports_mentors)
-#             RM_Mentee_List.append(reports_mentees)
-#
-#
-# past_reports_mentees = RM_Mentee_List
-# past_reports_mentors = RM_Mentor_List
-# print past_reports_mentees
-# print past_reports_mentors
-#
-# RM_file.close()
-#
-#
-# # with open("partnership_report.csv", "r") as PR_file:
-# #     PR_reader = csv.reader(PR_file)
-# #     next(PR_file, None)
-# #     count = {}
-# #     new_count = {}
-# #     for rows in PR_reader:
-# #         partnership_mentors = rows[2].title()
-# #         partnership_mentees = rows[5].title()
-# #         sdate = rows[0]
-# #         count.setdefault(sdate, 0)
-# #         count[sdate] += 1
-# #         if partnership_mentees in past_reports_mentees and partnership_mentors in past_reports_mentors:
-# #             # RM_date = rows[15]
-# #             Start_date = rows[0]
-# #             new_count.setdefault(Start_date, 0)
-# #             new_count[Start_date] += 1
-# #     print new_count
-# #     print count
-# with open ("pastRMs.csv", "r") as RM_file:
-#     with open("partnership_report.csv", "r") as PR_file:
-#         PR_reader = csv.reader(PR_file)
-#         RM_reader = csv.reader(RM_file)
-#         next(PR_file, None)
-#         next(RM_reader, None)
-#         for rows in PR_reader:
-#             partnership_mentors = rows[2].title()
-#             partnership_mentees = rows[5].title()
-#             if partnership_mentees in past_reports_mentees and partnership_mentors in past_reports_mentors:
+#This function is to check whether two documents match - or the functions are working properly
+# with open("partnership_report.csv", "r") as PR_file:
+#     PR_reader = csv.reader(PR_file)
+#     next(PR_file, None)
+#     count = {}
+#     new_count = {}
+#     for rows in PR_reader:
+#         partnership_mentors = rows[2].title()
+#         partnership_mentees = rows[5].title()
+#         sdate = rows[0]
+#         count.setdefault(sdate, 0)
+#         count[sdate] += 1
+#         for rows in RM_lists:
+#             if partnership_mentees in rows[1] and partnership_mentors in rows[0]:
+#                 # RM_date = rows[15]
 #                 Start_date = rows[0]
-#
-#                 # print Start_date, partnership_mentors, partnership_mentees
-#                     # if partnership_mentees == reports_mentees and partnership_mentors == reports_mentors:
-#                     #     print lines[15]
-#                     #
+#                 new_count.setdefault(Start_date, 0)
+#                 new_count[Start_date] += 1
+#         print new_count
+#         print count
