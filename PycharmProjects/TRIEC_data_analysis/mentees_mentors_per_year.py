9e8ad1cd542dddaf8f7a02085c2bac4716ca6268
peaceandiago <paola.santiago@mail.utoronto.ca>
2016-07-07 09:54:01 -0400

    finished a function

--- PycharmProjects/TRIEC_data_analysis/mentees_mentors_per_year.py
+++ PycharmProjects/TRIEC_data_analysis/mentees_mentors_per_year.py
@@ -5,27 +5,36 @@ import csv
 from datetime import timedelta, date
 import datetime
 
-data_out = open("mentees_all_attributes.csv", "rU")
-reader = csv.reader(data_out)
-next(reader,None)
-
-
-def create_dates(year):
-    start_date = datetime.date(year, 04, 01)
-    end_date = datetime.date(year, 03, 31)
-    return start_date, end_date
-
-
-
-def createJobCount():
+def createJobCountMentees(YEARS):
     """
     Function to take start and end date and compare to file to return the occupation
     :return: counts for each occupation for each job
     """
+    data_out = open("mentees_all_attributes.csv", "rU")
+    reader = csv.reader(data_out)
+    next(reader, None)
     job_count = {}
+    start_date = datetime.date(YEARS, 04, 01)
+    end_date = datetime.date(YEARS + 1, 03, 31)
     for rows in reader:
         date = datetime.datetime.strptime(rows[3], '%Y-%m-%d %H:%M:%S').date()
         if start_date <= date <= end_date:
             job_count.setdefault(rows[7], 0)
             job_count[rows[7]] += 1
-    print job_count
+    return job_count
+
+
+fiscalyear2010_2011_dict = createJobCountMentees(2010)
+
+def writetoFile(dict, file_name):
+    """
+    :param dict: the dictionary for each fiscal year containing the job counts
+    :param file_name: any name given to the written csv
+    :return: csv file
+    """
+    writer = csv.writer(open(file_name, "wb"))
+    for key, value in dict.items():
+        writer.writerow([key, value])
+    return writer
+
+fiscalyear2010_2011 = writetoFile(fiscalyear2010_2011_dict, "example.csv")
